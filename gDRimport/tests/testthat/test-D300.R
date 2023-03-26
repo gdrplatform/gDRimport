4a8f79716950f6006af4b92a080691646afb6858
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2023-03-26 23:14:40 +0200

    test: clean-up/simplify tests for D300

--- gDRimport/tests/testthat/test-D300.R
+++ gDRimport/tests/testthat/test-D300.R
@@ -1,18 +1,20 @@
 context("import_doses")
+td3 <- get_test_data3()
 
 test_that("parse_D300_xml", {
   
   # get test_data3
-  td3 <- get_test_data3()
-  
+ 
   # valid output returned for the D300 96 well plate example
-  dose_df <- parse_D300_xml(td3$d300_96w_file)
-  ref_dose_df <- readRDS(td3$ref_d300_96w_file)
+  fs <- td3[["f_96w"]]
+  dose_df <- parse_D300_xml(fs[["d300"]])
+  ref_dose_df <- readRDS(fs[["ref_d300"]])
   expect_identical(dose_df, ref_dose_df)
   
   # valid output returned for the D300 348 well plate example
-  dose_df <- parse_D300_xml(td3$d300_384w_file)
-  ref_dose_df <- readRDS(td3$ref_d300_384w_file)
+  fs2 <- td3[["f_384w"]]
+  dose_df <- parse_D300_xml(fs[["d300"]])
+  ref_dose_df <- readRDS(fs[["ref_d300"]])
   expect_identical(dose_df, ref_dose_df)
   
   # expected error(s) returned
@@ -21,44 +23,53 @@ test_that("parse_D300_xml", {
   
 })
 
+test_that("gDRimport:::parse_D300_metadata_file works as expected", {
+  
+  fs <- td3[["f_96w"]]
+  Gnum_96w_file <- gDRimport:::parse_D300_metadata_file(fs$Gnum) 
+  ref_Gnum_96w_file <- readRDS(fs$ref_Gnum)
+  expect_equal(Gnum_96w_file, ref_Gnum_96w_file)
+  
+  fs <- td3[["f_384w"]]
+  Gnum_96w_file <- gDRimport:::parse_D300_metadata_file(fs$Gnum) 
+  ref_Gnum_96w_file <- readRDS(fs$ref_Gnum)
+  expect_equal(Gnum_96w_file, ref_Gnum_96w_file)
+})
+
+
+
 test_that("import_D300", {
   
-  # get test_data3
-  td3 <- get_test_data3()
-  
-  # set directories for output and reference
-  dest_paths <- list(td3$dest_path_d300_96w,  
-                     td3$dest_path_d300_384w)
-  ref_paths <- list(td3$ref_output_path_d300_96w,  
-                     td3$ref_output_path_d300_384w)
-  d300_files <- list(td3$d300_96w_file,  
-                     td3$d300_384w_file)
-  Gnum_files <- list(td3$Gnum_96w_file,  
-                     td3$Gnum_384w_file)  
+  on.exit({
+    lapply(names(td3), function(x) {
+      unlink(td3[[x]][["dest_path"]], recursive = TRUE)
+    })
+  })
   
   #for 96 and 384 well plates
-  for (k in seq_len(length(dest_paths))) {
+  for (k in seq_along(td3)) {
     
     # validate output files for D300 examples
-    dest_path <- dest_paths[[k]]
-    ref_path <- ref_paths[[k]]
-    D300_file <- d300_files[[k]]
-    Gnum_file <- Gnum_files[[k]]
-    
+    fs <- td3[[k]]
+    dest_path <- fs$dest_path
+    ref_path <- fs$ref_output_path
+    D300_file <- fs$d300
+    Gnum_file <- fs$Gnum
+   
     # create directory if not existing
     if (!file.exists(dest_path)) {
       dir.create(dest_path, recursive = TRUE)
     } 
-    # clean files from output directory 
-    unlink(file.path(dest_path, "*"))
+    
     # run import_D300
     import_D300(D300_file, Gnum_file, dest_path)
     
     # test every output file against reference file 
-    list_files <- list.files(path = dest_path)
-    for (i in seq_len(length(list_files))) {
-      output_file_path <- file.path(dest_path, list_files[i])
-      ref_file_path <- file.path(ref_path, list_files[i])
+    fs <- list.files(path = dest_path)
+    idx <- c(1, length(fs)) # test with first and last file
+    for (i in idx) {
+      output_file_path <- file.path(dest_path, fs[i])
+      ref_file_path <- file.path(ref_path, fs[i])
       #load sheets
       output_sheets <- readxl::excel_sheets(output_file_path)
       ref_sheets <- readxl::excel_sheets(ref_file_path) 
@@ -89,26 +100,6 @@ test_that("get_conversion_factor  works as expected", {
 test_that("convert_units works as expected", {
   expect_equal(gDRimport:::convert_units(1000, from = "mL", to = "µL"), 1000000)
 })
-
-#######
-# Gnum
-#######
-
-test_that("gDRimport:::parse_D300_metadata_file works as expected", {
-  
-  # get test_data3
-  td3 <- get_test_data3()
-  
-  # test Gnum files
-  Gnum_96w_file <- gDRimport:::parse_D300_metadata_file(td3$Gnum_96w_file) 
-  ref_Gnum_96w_file <- readRDS(td3$ref_Gnum_96w_file)
-  expect_equal(Gnum_96w_file, ref_Gnum_96w_file)
-  
-  Gnum_384w_file <- gDRimport:::parse_D300_metadata_file(td3$Gnum_384w_file) 
-  ref_Gnum_384w_file <- readRDS(td3$ref_Gnum_384w_file)
-  expect_equal(Gnum_384w_file, ref_Gnum_384w_file)
-})
-
 
 #######
 # Utils
