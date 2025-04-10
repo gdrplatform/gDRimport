77eb04d974cea11f43cbf86c78cd1c9161066b8b
Dariusz Scigocki <daro.scig@gmail.com>
2025-04-10 17:12:00 +0000

    replaced with_mock with with_mocked_bindings

--- tests/testthat/test-pset_to_gdrDF.R
+++ tests/testthat/test-pset_to_gdrDF.R
@@ -13,8 +13,9 @@ read_mocked_PSets <- function(canonical = FALSE) {
 }
 
 test_that("getPSet works as expected", {
-  pset <- testthat::with_mock(
-    `PharmacoGx::availablePSets` = read_mocked_PSets,
+  pset <- testthat::with_mocked_bindings(
+    availablePSets = read_mocked_PSets,
+    .package = "PharmacoGx",
     suppressMessages(getPSet(
       "Tavor_2020", psetDir = system.file("extdata/pset", package = "gDRimport")
     ))
@@ -31,8 +32,9 @@ test_that("setEnvForPSet works as expected", {
 })
 
 test_that(".extractDoseResponse works as expected", {
-  pset <- testthat::with_mock(
-    `PharmacoGx::availablePSets` = read_mocked_PSets,
+  pset <- testthat::with_mocked_bindings(
+    availablePSets = read_mocked_PSets,
+    .package = "PharmacoGx",
     suppressMessages(getPSet(
       "Tavor_2020", psetDir = system.file("extdata/pset", package = "gDRimport")
     ))
@@ -45,8 +47,9 @@ test_that(".extractDoseResponse works as expected", {
 })
 
 test_that(".extractDoseResponse, .removeNegatives, and .createPseudoData work as expected", {
-  pset <- testthat::with_mock(
-    `PharmacoGx::availablePSets` = read_mocked_PSets,
+  pset <- testthat::with_mocked_bindings(
+    availablePSets = read_mocked_PSets,
+    .package = "PharmacoGx",
     suppressMessages(getPSet(
       "Tavor_2020", psetDir = system.file("extdata/pset", package = "gDRimport")
     ))
@@ -66,8 +69,9 @@ test_that(".extractDoseResponse, .removeNegatives, and .createPseudoData work as
 
 test_that("convert_pset_to_df works as expected", {
   on.exit(gDRutils::reset_env_identifiers())
-  pset <- testthat::with_mock(
-    `PharmacoGx::availablePSets` = read_mocked_PSets,
+  pset <- testthat::with_mocked_bindings(
+    availablePSets = read_mocked_PSets,
+    .package = "PharmacoGx",
     suppressMessages(getPSet(
       "Tavor_2020", psetDir = system.file("extdata/pset", package = "gDRimport")
     ))
